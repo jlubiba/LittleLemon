48d021ca1034f497f8a06a6abea0a90d519c19d2
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-09-17 04:47:51 +0100

    Adding models and connecting mysql db.

---
+++ LittleLemonP/restaurant/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Booking(models.Model):
+    name = models.CharField(max_length=225)
+    no_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
