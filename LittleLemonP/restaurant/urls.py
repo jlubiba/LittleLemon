ca315df4186dd1815211d64756ba3bc9675983ce
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-09-19 11:52:50 +0100

    added menu model related api, serializers, views

--- LittleLemonP/restaurant/urls.py
+++ LittleLemonP/restaurant/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
+router.register("booking", views.BookingViewSet, basename="bookings")
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view(), name="menu_item"),
+    path("menu/items", views.MenuItemView.as_view(), name="menu_items"),
 ]
+
+urlpatterns = router.urls
--- LittleLemonP/restaurant/views.py
+++ LittleLemonP/restaurant/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, "restaurant/index.html", context)
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
