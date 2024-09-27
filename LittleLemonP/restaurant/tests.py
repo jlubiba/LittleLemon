25143c8a33c4a6b3367d0387f08d6bd9cbe4279c
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-09-27 05:16:21 +0100

    Removing commented codes to keep it all clean.

--- LittleLemonP/restaurant/tests.py
+++ LittleLemonP/restaurant/tests.py
@@ -8,11 +8,3 @@ class MenuItemsTest(TestCase):
         itm = item.get_item()
         
         self.assertEqual(itm, "IceCream : 30")
-
-# class MenuTest(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.menu = Menu.objects.create(title="IceCream", price=30, inventory=50)
-        
-#     def test_menu_output(self):
-#         self.assertEqual(self.menu, "IceCream : 30")
--- LittleLemonP/restaurant/urls.py
+++ LittleLemonP/restaurant/urls.py
@@ -5,13 +5,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"booking", views.BookingViewSet, basename="bookings")
-router.register(r"menu", views.BookingViewSet, basename="menu")
-# router.register(r"home", views.index, basename="home")
+router.register(r"menu", views.BookingViewSet, basename="menu_items")
 
 urlpatterns = [
     path("", views.index, name="home"),
-    # path("menu/<int:pk>", views.SingleMenuItemView.as_view(), name="menu_item"),
-    # path("menu/items", views.MenuItemView.as_view(), name="menu_items"),
     path("api-token-auth/", obtain_auth_token),
 ]
 
