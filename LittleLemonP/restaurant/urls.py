4e39849b34ac9656c7f41800befe70643483e7f3
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-09-27 05:14:08 +0100

    Changingrestaurant app views and urls to accomodate paths and routers.

--- LittleLemonP/restaurant/urls.py
+++ LittleLemonP/restaurant/urls.py
@@ -4,14 +4,15 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter(trailing_slash=False)
-router.register("booking", views.BookingViewSet, basename="bookings")
-router.register("menu", views.BookingViewSet, basename="menu")
+router.register(r"booking", views.BookingViewSet, basename="bookings")
+router.register(r"menu", views.BookingViewSet, basename="menu")
+# router.register(r"home", views.index, basename="home")
 
-# urlpatterns = [
-#     path("", views.index, name="home"),
-#     path("menu/<int:pk>", views.SingleMenuItemView.as_view(), name="menu_item"),
-#     path("menu/items", views.MenuItemView.as_view(), name="menu_items"),
-#     path("api-token-auth/", obtain_auth_token),
-# ]
+urlpatterns = [
+    path("", views.index, name="home"),
+    # path("menu/<int:pk>", views.SingleMenuItemView.as_view(), name="menu_item"),
+    # path("menu/items", views.MenuItemView.as_view(), name="menu_items"),
+    path("api-token-auth/", obtain_auth_token),
+]
 
-urlpatterns = router.urls
+urlpatterns += router.urls
--- LittleLemonP/restaurant/views.py
+++ LittleLemonP/restaurant/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics, viewsets, filters
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 # Create your views here.
+@api_view(['GET'])
+@throttle_classes([AnonRateThrottle])
 def index(request):
     context = {}
     return render(request, "restaurant/index.html", context)
-
 
 # class MenuItemView(generics.ListCreateAPIView):
 #     queryset = Menu.objects.all()
 #     serializer_class = MenuSerializer
 #     permission_classes = [IsAuthenticatedOrReadOnly]
-    
-            
-
 
 # class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
 #     queryset = Menu.objects.all()
